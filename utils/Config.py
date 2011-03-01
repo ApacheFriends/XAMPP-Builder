aa8dc7841d5b5d4fec4e022732a6ceaa23bd2ed3
Christian Speich <kleinweby@kleinweby.de>
2011-03-01 17:26:47 +0100

    Paths returned by the config object are now absolute

--- utils/Config.py
+++ utils/Config.py
@@ -10,20 +10,40 @@
 
 from ConfigParser import SafeConfigParser
 
+import os.path
+
 class Config(object):
 
-    def __init__(self, config_file, platform):
+    def __init__(self, config_file, platform, rel_base=None):
         self.config_file = config_file
         self.platform = platform
 
         self.configParser = SafeConfigParser()
         self.configParser.read([self.config_file])
+        
+        '''
+          If no base for relative paths is given,
+          we use the dir in which the config resides
+        '''
+        if rel_base is None:
+            rel_base = os.path.dirname(config_file)
+        
+        self.rel_base = os.path.abspath(rel_base)
 
     @property
     def archivesPath(self):
-        return self.configParser.get('XAMPP Builder', 'archives')
+        return self.preparePath(self.configParser.get('XAMPP Builder', 'archives'))
 
     @property
     def build_dir(self):
-        return self.configParser.get('XAMPP Builder', 'build_dir')
+        return self.preparePath(self.configParser.get('XAMPP Builder', 'build_dir'))
+    
+    '''
+      Returns an normalised and absolut path for any path given.
+    '''
+    def preparePath(self, path):
+        if not os.path.isabs(path):
+            path = os.path.join(self.rel_base, path)
+        
+        return os.path.abspath(path)
 
