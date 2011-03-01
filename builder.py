7459eb0d8181536842e777a81126dd88695d76fd
Christian Speich <kleinweby@kleinweby.de>
2011-03-01 07:51:49 +0100

    Require python 2.6 or later

--- builder.py
+++ builder.py
@@ -15,6 +15,10 @@ import os.path
 ''' Add the dir of this file to the PATH to get utils and components to include '''
 sys.path.append(os.path.dirname(__file__))
 
+''' Require to run with python 2.6 or later '''
+if not sys.hexversion >= 0x020600F0:
+    raise RuntimeError('Python 2.6 or later is required')
+
 from utils.Builder import Builder
 
 if __name__ == '__main__':
