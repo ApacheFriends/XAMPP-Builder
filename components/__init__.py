6799933a3d05c5c34d077a0e79b8f0d703ebb295
Christian Speich <kleinweby@kleinweby.de>
2012-01-19 23:25:27 +0100

    Add libpng

--- components/__init__.py
+++ components/__init__.py
@@ -8,6 +8,7 @@
 import components.zlib.ZLib
 import components.openssl.OpenSSL
 import components.libltdl.LibLTDL
+import components.libpng.LibPNG
 import components.ncurses.Ncurses
 import components.sqlite.SQLite
 import components.expat.Expat
@@ -17,14 +18,15 @@ import components.proftpd.ProFTPd
 import components.perl.Perl
 
 KNOWN_COMPONENTS = [
-    components.zlib.ZLib,
-    components.openssl.OpenSSL,
-#    components.libltdl.LibLTDL,
-    components.ncurses.Ncurses,
-    components.sqlite.SQLite,
-    components.expat.Expat,
-    components.apache.Apache,
-    components.mysql.MySQL,
-    components.proftpd.ProFTPd,
-    components.perl.Perl
+	components.zlib.ZLib,
+	components.openssl.OpenSSL,
+#	components.libltdl.LibLTDL,
+	components.libpng.LibPNG
+	components.ncurses.Ncurses,
+	components.sqlite.SQLite,
+	components.expat.Expat,
+	components.apache.Apache,
+	components.mysql.MySQL,
+	components.proftpd.ProFTPd,
+	components.perl.Perl
 ]
---
+++ components/libpng/LibPNG.py
@@ -0,0 +1,24 @@
+"""
+  XAMPP Builder
+  Copyright 2011-2012 Apache Friends, GPLv2+ licensed
+  ===================================================
+
+  The LibPNG component.
+"""
+
+from utils.Component import Component
+from utils.Dependency import Dependency
+
+import os.path
+
+class LibPNG(Component):
+
+    def __init__(self, config):
+        super(LibPNG, self).__init__('LibPNG', os.path.dirname(__file__), config)
+
+        self.download_url = 'http://switch.dl.sourceforge.net/sourceforge/libpng/libpng-%s.tar.bz2' % self.version
+
+        self.dependencies = [
+            Dependency('ZLib', configureFlags=["--with-z=${COMPONENT_PATH}"]),
+        ]
+
