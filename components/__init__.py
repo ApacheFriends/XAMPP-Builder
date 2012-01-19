e6c50403e83a91486754f483d5a054d4df126c95
Christian Speich <kleinweby@kleinweby.de>
2012-01-20 00:14:14 +0100

    Added LibXML 2.7.8

--- components/__init__.py
+++ components/__init__.py
@@ -10,6 +10,7 @@ import components.openssl.OpenSSL
 import components.libjpeg.LibJPEG
 import components.libltdl.LibLTDL
 import components.libpng.LibPNG
+import components.libxml.LibXML
 import components.ncurses.Ncurses
 import components.sqlite.SQLite
 import components.expat.Expat
@@ -24,6 +25,7 @@ KNOWN_COMPONENTS = [
 	components.libjpeg.LibJPEG,
 #	components.libltdl.LibLTDL,
 	components.libpng.LibPNG,
+	components.libxml.LibXML,
 	components.ncurses.Ncurses,
 	components.sqlite.SQLite,
 	components.expat.Expat,
---
+++ components/libxml/LibXML.py
@@ -0,0 +1,22 @@
+"""
+  XAMPP Builder
+  Copyright 2011-2012 Apache Friends, GPLv2+ licensed
+  ===================================================
+
+  The LibXML component.
+"""
+
+from utils.Component import Component
+from utils.Dependency import Dependency
+
+import os
+import os.path
+
+class LibXML(Component):
+
+	def __init__(self, config):
+		super(LibXML, self).__init__('LibXML', os.path.dirname(__file__), config)
+
+		self.download_url = 'ftp://xmlsoft.org/libxml2/libxml2-%s.tar.gz' % self.version
+		
+
