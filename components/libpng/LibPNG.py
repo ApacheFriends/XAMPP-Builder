af05e43e1b907262832482710f1d145815acacd7
Christian Speich <kleinweby@kleinweby.de>
2012-01-19 23:29:29 +0100

    Converted intends to tabs

--- components/libpng/LibPNG.py
+++ components/libpng/LibPNG.py
@@ -13,12 +13,12 @@ import os.path
 
 class LibPNG(Component):
 
-    def __init__(self, config):
-        super(LibPNG, self).__init__('LibPNG', os.path.dirname(__file__), config)
+	def __init__(self, config):
+		super(LibPNG, self).__init__('LibPNG', os.path.dirname(__file__), config)
 
-        self.download_url = 'http://switch.dl.sourceforge.net/sourceforge/libpng/libpng-%s.tar.bz2' % self.version
+		self.download_url = 'http://switch.dl.sourceforge.net/sourceforge/libpng/libpng-%s.tar.bz2' % self.version
 
-        self.dependencies = [
-            Dependency('ZLib', configureFlags=["--with-z=${COMPONENT_PATH}"]),
-        ]
+		self.dependencies = [
+			Dependency('ZLib', configureFlags=["--with-z=${COMPONENT_PATH}"]),
+		]
 
